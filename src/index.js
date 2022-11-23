79642d6443def44032e059fa4d2a7903e4e9045f
Laura Portillo <lauraportillo.ark@gmail.com>
2022-11-23 10:23:49 +0100

    add bikes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import { HeroesApp } from './HeroesApp';
+// import { HeroesApp } from './HeroesApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <HeroesApp />
+      {/* <HeroesApp /> */}
     </HashRouter>
   </React.StrictMode>
 );
