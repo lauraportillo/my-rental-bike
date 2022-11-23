046e47715b36f3d1e62dc5461e8dd696abe71e22
Laura Portillo <lauraportillo.ark@gmail.com>
2022-11-23 11:03:26 +0100

    structure bikes object

--- src/bikes/data/bikes.js
+++ src/bikes/data/bikes.js
@@ -1,10 +1,50 @@
 export const bikes = [
     {
-        'id': 'dc-batman',
-        'superhero': 'Batman',
-        'publisher': 'DC Comics',
-        'alter_ego': 'Bruce Wayne',
-        'first_appearance': 'Detective Comics #27',
-        'characters': 'Bruce Wayne'
-    }
+        'id': 'electric-capri-berlin-black',
+        'type': 'Electric',
+        'name': 'Capri berlin black'
+    },
+    {
+        'id': 'electric-capri-berlin-cream',
+        'type': 'Electric',
+        'name': 'Capri berlin cream'
+    },
+    {
+        'id': 'electric-capri-metz-black',
+        'type': 'Electric',
+        'name': 'Capri metz black'
+    },
+    {
+        'id': 'road-troy-black',
+        'type': 'Road',
+        'name': 'Troy black'
+    },
+    {
+        'id': 'road-troy-highland-gray',
+        'type': 'Road',
+        'name': 'Troy highland gray'
+    },
+    {
+        'id': 'road-troy-windsor-blue',
+        'type': 'Road',
+        'name': 'Troy windsor blue'
+    },
+
+    {
+        'id': 'city-capri-carolina-pink',
+        'type': 'City',
+        'name': 'Capri carolina pink'
+    },
+    {
+        'id': 'city-capri-gracia-cream',
+        'type': 'City',
+        'name': 'Capri gracia cream'
+    },
+    {
+        'id': 'city-capri-valentina-aquamarina',
+        'type': 'City',
+        'name': 'Capri valentina aquamarina'
+    },
+
+
 ]
