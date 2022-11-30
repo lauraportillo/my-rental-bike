1ca0e531eb4c2196b4fe1b8dfb7677829d25eb61
Laura Portillo <lauraportillo.ark@gmail.com>
2022-11-30 13:48:27 +0100

    update form

--- src/auth/pages/RentPage.jsx
+++ src/auth/pages/RentPage.jsx
@@ -2,7 +2,7 @@ import { useForm } from '../../hooks/useForm';
 import { Navbar } from '../../ui';
 
 export const RentPage = () => {
-  const { displayName, email, onInputChange } = useForm();
+  const { displayName, email, telephone, onInputChange } = useForm();
 
   const onSearchSubmit = (ev) => {
     ev.preventDefault();
@@ -17,14 +17,40 @@ export const RentPage = () => {
 
         <form onSubmit={onSearchSubmit} className="col-6">
           <div className="form-group">
-            <label>Full Name</label>
+            <label htmlFor="displayName">Full name</label>
             <input
               type="text"
               placeholder="Your Name"
               name="displayName"
               value={displayName}
               onChange={onInputChange}
-              className="form-control"
+              className="form-control mt-1 mb-2"
+              autoComplete="off"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              placeholder="email@google.com"
+              name="email"
+              value={email}
+              onChange={onInputChange}
+              className="form-control mt-1 mb-2"
+              autoComplete="off"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="telephone">Phone number</label>
+            <input
+              type="telephone"
+              placeholder="123456789"
+              name="telephone"
+              value={telephone}
+              onChange={onInputChange}
+              className="form-control mt-1 mb-2"
               autoComplete="off"
             />
           </div>
