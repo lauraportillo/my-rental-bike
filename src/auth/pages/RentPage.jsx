757febdc06f7685793c156d98451df8c5b252190
Laura Portillo <lauraportillo.ark@gmail.com>
2022-11-30 13:00:27 +0100

    add form to rent page

--- src/auth/pages/RentPage.jsx
+++ src/auth/pages/RentPage.jsx
@@ -1,12 +1,34 @@
+import { useForm } from '../../hooks/useForm';
 import { Navbar } from '../../ui';
 
 export const RentPage = () => {
+  const { displayName, email, onInputChange } = useForm();
+
+  const onSearchSubmit = (ev) => {
+    ev.preventDefault();
+  };
+
   return (
     <>
       <Navbar />
 
       <div className="container">
         <h1 className="mt-3 mb-3">Rent your bike</h1>
+
+        <form onSubmit={onSearchSubmit} className="col-6">
+          <div className="form-group">
+            <label>Full Name</label>
+            <input
+              type="text"
+              placeholder="Your Name"
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
+              className="form-control"
+              autoComplete="off"
+            />
+          </div>
+        </form>
       </div>
     </>
   );
